4af3473ce3fc8fde442a8ba8459e6982f98760e5
vishal1605 <vishalgt581@gmail.com>
2022-08-15 11:55:33 +0530

    project started

---
+++ experimentProject123/experimentProject/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using experimentProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace experimentProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration Configuration;
+
+        public HomeController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Insert(string requestData)
+        {
+            dynamic? jsonData = JsonConvert.DeserializeObject(requestData);
+            DateTime date = DateTime.Parse(jsonData["registerDate"].ToString());
+             var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
+             var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+            Console.WriteLine(firstDayOfMonth);
+
+            //var sqlPara = new
+            //{
+            //    email= jsonData["email"].ToString(),
+            //    name = jsonData["name"].ToString(),
+            //    psw = jsonData["psw"].ToString(),
+            //    registerDate = DateTime.Parse(jsonData["registerDate"].ToString()),
+            //    firstDay = DateTime.Parse(jsonData["firstDay"].ToString()),
+            //    lastDay = DateTime.Parse(jsonData["lastDay"].ToString()),
+            //};
+            //Console.WriteLine(sqlPara);
+            //string query = "insert into tbluser(email,name,psw, registerDate) values (@email, @name, @psw, @registerDate)";
+            //SqlConnection con = new SqlConnection(this.Configuration.GetConnectionString("DefaultConnection"));
+            var row = 3 ;
+
+            return new JsonResult(row);
+        }
+    }
+}
---
+++ experimentProject123/experimentProject/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿document.getElementById('insert-form').addEventListener('submit', formSubmit);
+function formSubmit(e) {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const email = data.get('Email');
+    const name = data.get('Name');
+    const psw = data.get('Psw');
+    const currentMonth = new Date("2022-02-15")
+    console.log(currentMonth);
+    const registerDate = new Date(currentMonth);
+    
+    var firstDay = new Date(registerDate.getFullYear(), registerDate.getMonth(), 2);
+    var lastDay = new Date(registerDate.getFullYear(), registerDate.getMonth() + 1, 1);
+    var formData = {
+        email, name, psw, registerDate, firstDay, lastDay
+    }
+    $.ajax({
+        type: 'POST',
+        url: "/Home/Insert",
+        data: {
+            requestData: JSON.stringify(formData)
+        },
+        
+        success: function (resultData) {
+            console.log(resultData);
+           
+        }
+    });
+    console.log(formData);
+    
+}
